709a2ec36a0881a77aab465d68494b27fc893e38
rishabhofficial005-alt <rishabhofficial005@gmail.com>
2026-08-12 15:04:46 +0530

    Time: 3 ms (86.05%), Space: 43.6 MB (15.39%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character> st = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            if (!st.isEmpty() &&
+                ((ch == ']' && st.peek() == '[') ||
+                 (ch == '}' && st.peek() == '{') ||
+                 (ch == ')' && st.peek() == '('))) {
+
+                st.pop();
+            }
+            else {
+                st.push(ch);
+            }
+        }
+
+        if (!st.isEmpty()) {
+            return false;
+        }
+
+        return true;
+    }
+}
