90c6ee4d0ec7fc0d04bd8a34b9d3a519f08eee7f
rishabhofficial005-alt <rishabhofficial005@gmail.com>
2026-08-12 14:43:33 +0530

    Time: 3 ms (95.73%), Space: 44.5 MB (91.46%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minLength(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(char ch: s.toCharArray()){
+            int len=sb.length();
+            if(len>0 && ((sb.charAt(len-1)=='A' && ch=='B')||(sb.charAt(len-1)=='C'&& ch=='D'))){
+                sb.deleteCharAt(len-1);
+            }
+            else{
+                sb.append(ch);
+            }
+        }
+        return sb.length();
+    }
+}
