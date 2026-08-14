2e1bf10fc57ba68bdeacbf202bf72798cec94355
rishabhofficial005-alt <rishabhofficial005@gmail.com>
2026-08-14 09:51:50 +0530

    Time: 0 ms (100%), Space: 42.7 MB (70.41%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -9,25 +9,29 @@ class MyQueue {
     }
     
     public void push(int x) {
-       while(!st1.isEmpty()){
-        st2.push(st1.pop());
-       }
        st1.push(x);
-       while(!st2.isEmpty()){
-        st1.push(st2.pop());
-       } 
     }
     
     public int pop() {
-        return st1.pop();
+        if(st2.isEmpty()){
+            while(!st1.isEmpty()){
+                st2.push(st1.pop());
+            }
+        }
+        return st2.pop();
     }
     
     public int peek() {
-        return st1.peek();
+        if(st2.isEmpty()){
+            while(!st1.isEmpty()){
+                st2.push(st1.pop());
+            }
+        }
+        return st2.peek();
     }
     
     public boolean empty() {
-        return st1.isEmpty();
+        return st1.isEmpty() && st2.isEmpty();
     }
 }
 
