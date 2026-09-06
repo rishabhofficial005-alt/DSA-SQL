1b93f416a198be854d464b7c6538e1e218cabce8
rishabhofficial005-alt <rishabhofficial005@gmail.com>
2026-09-06 22:32:36 +0530

    Time: 0 ms (100%), Space: 44.9 MB (12.36%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -3,10 +3,12 @@ class Solution {
         int len=nums.length;
         int start=0;
         int end=len-1;
+        int ans=len;
         while(start<=end){
             int mid=(start)+(end-start)/2;
-            if(nums[mid]==target){
-                return mid;
+            if(nums[mid]>=target){
+                ans=mid;
+                end=mid-1;
             }
             else if(nums[mid]<target){
                 start=mid+1;
@@ -15,6 +17,6 @@ class Solution {
                 end=mid-1;
             }
         }
-        return start;   
+        return ans;   
     }
 }
