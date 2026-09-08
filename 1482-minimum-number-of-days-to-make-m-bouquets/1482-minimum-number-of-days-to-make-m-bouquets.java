49e64117f6b3f2e6c6f23e35616cc6edcd3115e7
rishabhofficial005-alt <rishabhofficial005@gmail.com>
2026-09-08 23:01:27 +0530

    Time: 16 ms (91.66%), Space: 82.7 MB (47.76%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,39 +1,44 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
-        if(m*k>bloomDay.length){
-            return -1;
-        }
-        int low=Integer.MAX_VALUE;
-        int high=Integer.MIN_VALUE;
-        int ans=-1;
-        for(int day : bloomDay){
-            low=Math.min(low,day);
-            high=Math.max(high,day);
-        }
-        while(low<=high){
-            int mid=(low)+(high-low)/2;
-            int count=0;
-            int bookey=0;
-            for(int i=0;i<bloomDay.length;i++){
-                if(bloomDay[i]<=mid){
-                   count++;
-                   if(count==k){
-                    bookey++;
-                    count=0;
-                   }
+    if ((long)m * k > bloomDay.length) {
+    return -1;
+}
+     int start=Integer.MAX_VALUE;
+     int end=Integer.MIN_VALUE;
+     for(int day: bloomDay){
+        start=Math.min(start,day);
+        end=Math.max(end,day);
+     }
+     int ans=end;
+    
+     while(start<=end){
+        int b=0; int c=0;
+        int mid=(start)+(end-start)/2;
+        for(int bloomday: bloomDay){
+            if(bloomday<=mid){
+                c++;
+                if(c==k){
+                    b++;
+                    c=0;
                 }
-                else{
-                    count=0;
-                }
-            }
-            if(bookey>=m){
-                ans=mid;
-                high=mid-1;
+                
             }
             else{
-                low=mid+1;
+                    c=0;
             }
+            
+
         }
-        return ans;
+        if(b>=m){
+            ans=mid;
+            end=mid-1;
+        }
+        else{
+            start=mid+1;
+        }
+        
+
+     }
+     return ans;   
     }
 }
