0221a061c691eb303e0c9acb42776ef2c86237c8
rishabhofficial005-alt <rishabhofficial005@gmail.com>
2026-09-13 15:28:41 +0530

    Time: 4 ms (94.12%), Space: 51.3 MB (9.1%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
@@ -1,27 +1,30 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
-        int n=nums.length;
-        int currentmaxsum=0;
-        int currentminsum=0;
-        int maxsum=Integer.MIN_VALUE;
-        int minsum=Integer.MAX_VALUE;
-        int total=0;
+        int max_sum=Integer.MIN_VALUE;
+        int min_sum=Integer.MAX_VALUE;
+        int total_sum=0;
+        int current_sum_1=0;
+        int current_sum_2=0;
         for(int i=0;i<nums.length;i++){
-            currentmaxsum+=nums[i];
-            currentminsum+=nums[i];
-            maxsum=Math.max(currentmaxsum,maxsum);
-            minsum=Math.min(currentminsum,minsum);
-            if(currentmaxsum<0){
-                currentmaxsum=0;
-            }
-            if(currentminsum>0){
-                currentminsum=0;
-            }
-            total+=nums[i];
+            total_sum+=nums[i];
+
         }
-        if(maxsum<0){
-            return maxsum;
+        for(int i=0;i<nums.length;i++){
+            current_sum_1+=nums[i];
+            current_sum_2+=nums[i];
+            max_sum=Math.max(max_sum,current_sum_1);
+            min_sum=Math.min(min_sum,current_sum_2);
+            if(current_sum_1<0){
+                current_sum_1=0;
+            }
+            if(current_sum_2>0){
+                current_sum_2=0;
+            }
         }
-     return Math.max(maxsum,total-minsum);
+        if (max_sum < 0) {
+            return max_sum;
+        }
+        return Math.max(max_sum,total_sum-min_sum);
+        
     }
 }
