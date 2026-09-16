8481c7e57b3f7ecdcff74aa7a6b614298838d478
rishabhofficial005-alt <rishabhofficial005@gmail.com>
2026-09-17 00:44:38 +0530

    Time: 625 ms (8.1%), Space: 47.2 MB (30.62%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,33 +1,32 @@
 class Solution {
-    public boolean checksame(int []freq1,int []windowfreq){
+    public boolean Permutation(int freq1[],int freq2[]){
         for(int i=0;i<26;i++){
-            if(freq1[i]!=windowfreq[i]){
+            if(freq1[i]!=freq2[i]){
                 return false;
-            }
+            }    
         }
         return true;
     }
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> ans=new ArrayList<>();
+        ArrayList<Integer> ans=new ArrayList<>();
+        int window_length=p.length();
         int freq1[]=new int[26];
         for(int i=0;i<p.length();i++){
             freq1[p.charAt(i)-'a']++;
         }
-        int windowlength=p.length();
         for(int i=0;i<s.length();i++){
-            int windowfreq1[]=new int[26];
             int index=i;
             int windowindex=0;
-            while(index<s.length() && windowindex<windowlength){
-                windowfreq1[s.charAt(index)-'a']++;
+            int freq2[]=new int[26];
+            while(index<s.length() && windowindex<window_length){
+                freq2[s.charAt(index)-'a']++;
                 index++;
                 windowindex++;
             }
-            if(checksame(freq1,windowfreq1)){
+            if(Permutation(freq1,freq2)){
                 ans.add(i);
             }
         }
-        return ans; 
-        
+        return ans;
     }
 }
