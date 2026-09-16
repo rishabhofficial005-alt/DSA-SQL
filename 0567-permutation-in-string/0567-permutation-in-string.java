ab4e36e754aa26ccad2e3b42c99f9e4398572b39
rishabhofficial005-alt <rishabhofficial005@gmail.com>
2026-09-17 00:32:40 +0530

    Time: 172 ms (9.79%), Space: 47 MB (10.73%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,33 +1,31 @@
 class Solution {
-    public boolean CheckPermutation(int []windowfreq, int[] freq1){
+    public boolean Permutation(int []freq1,int []freq2){
         for(int i=0;i<26;i++){
-            if(windowfreq[i] != freq1[i]){
+            if(freq1[i]!=freq2[i]){
                 return false;
             }
         }
         return true;
     }
     public boolean checkInclusion(String s1, String s2) {
-        int windowfreq[]=new int [26];
-        
-        int windowlength=s1.length();
+        int window_length=s1.length();
+        int freq1[]=new int[26];
+       
         for(int i=0;i<s1.length();i++){
-            windowfreq[s1.charAt(i)-'a']++;
+            freq1[s1.charAt(i)-'a']++;
         }
         for(int i=0;i<s2.length();i++){
-            int index=i;
+            int minindex=i;
             int windowindex=0;
-            int freq1[]=new int[26];
-            while(index<s2.length() && windowindex<windowlength){
-                freq1[s2.charAt(index)-'a']++;
+            int freq2[]=new int[26];
+            while(minindex<s2.length() && windowindex<window_length){
+                freq2[s2.charAt(minindex)-'a']++;
+                minindex++;
                 windowindex++;
-                index++;
             }
-            if(CheckPermutation(windowfreq,freq1)){
-                return true;
+            if(Permutation(freq1,freq2)){
+               return true;
             }
-            
-
         }
         return false;
     }
