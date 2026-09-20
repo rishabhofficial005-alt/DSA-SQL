ab3de035b0c51f3477e62fb21fe39f369f37c65a
rishabhofficial005-alt <rishabhofficial005@gmail.com>
2026-09-20 16:05:58 +0530

    Time: 5 ms (40.51%), Space: 47.7 MB (11.97%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
@@ -1,25 +1,23 @@
-import java.util.*;
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        int n=grid.length;
-        HashSet<Integer> set= new HashSet<>();
-        int sum=0;
-        int repeating_element=-1;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(!set.contains(grid[i][j])){
-                    set.add(grid[i][j]);
+        int m=grid.length;
+        int n=grid[0].length;
+        int repeated_element=0;
+        HashSet<Integer> set=new HashSet<>();
+        int total_sum=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(set.contains(grid[i][j])){
+                    repeated_element=grid[i][j];
                 }
-                else{
-                    repeating_element=grid[i][j];
-                }
+                set.add(grid[i][j]);
+                total_sum+=grid[i][j];
 
-               
-                sum=sum+grid[i][j];
             }
         }
-       long actual_sum = (long)(n * n) * ((n * n) + 1) / 2;
-       int missing_value = (int)(actual_sum)- (sum - repeating_element);
-       return new int[]{repeating_element, missing_value};
+        int k=m*m ;
+        long actual_sum=(k*(k+1))/2;
+        int missing_element=(int) actual_sum-(total_sum-repeated_element);
+        return new int[] {repeated_element,missing_element};
     }
 }
